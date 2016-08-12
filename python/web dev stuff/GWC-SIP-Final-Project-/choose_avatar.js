af27816aeb6b27a62e2e5a3cd2decef5f153b6bf
espickermann <espickermann@users.noreply.github.com>
2016-08-12 14:22:09 -0700

    Add files via upload

---
+++ python/web dev stuff/GWC-SIP-Final-Project-/choose_avatar.js
@@ -0,0 +1,14 @@
+function avatar1(){
+	sessionStorage.avatar = 1;
+	console.log(sessionStorage.avatar);
+}
+
+function avatar2(){
+	sessionStorage.avatar = 2;
+	console.log(sessionStorage.avatar);
+}
+
+function avatar3(){
+	sessionStorage.avatar = 3;
+	console.log(sessionStorage.avatar);
+}
---
+++ python/web dev stuff/GWC-SIP-Final-Project-/enter_name_script.js
@@ -0,0 +1,59 @@
+function saveName(){
+	var elem = document.getElementById("name");
+	
+	
+	sessionStorage.yourname = elem.value;
+	
+	
+
+
+	console.log(sessionStorage.yourname);
+} 
+
+
+function add_3_shells(){
+		var score = parseInt(sessionStorage.friendship_points);
+		score +=3;
+		console.log(score);
+		sessionStorage.friendship_points = score;
+		
+}
+
+function add_2_shells(){
+		var score = parseInt(sessionStorage.friendship_points);
+		score += 2 ;
+		console.log(score);
+		sessionStorage.friendship_points = score;
+}
+
+function add_1_shell(){
+		var score = parseInt(sessionStorage.friendship_points);
+		score +=1 ;
+		console.log(score);
+		sessionStorage.friendship_points = score;
+}
+function draw_shells(){
+			console.log(sessionStorage.friendship_points);
+	
+	for (i = 0; i < sessionStorage.friendship_points; i++){
+		var shell_pic = document.createElement("img"); 
+		
+		shell_pic.setAttribute('src', 'shell.png');
+		shell_pic.setAttribute('style', 'width: 20px');
+		document.getElementById("narrow").appendChild(shell_pic);
+		//document.body.appendChild(shell_pic);
+}
+}
+function sayShells(){
+	if(sessionStorage.friendship_points == 1){
+		document.getElementById("shells").innerHTML = "You have "+sessionStorage.friendship_points+" shell!";
+	}else{
+		document.getElementById("shells").innerHTML = "You have "+sessionStorage.friendship_points+" shells!"
+	}
+}
+
+function resetShells(){
+	sessionStorage.friendship_points = 0;
+	console.log(sessionStorage.friendship_points);
+}
+	
